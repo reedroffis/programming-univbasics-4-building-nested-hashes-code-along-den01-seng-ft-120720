24efc81a2f653f24f97a9221db8d9fafd1793e68
Reed Roffis <reedroffis18@gmail.com>
2020-12-01 04:12:42 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,10 +2,10 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        
+        name: "Lord Montague", age: "53"
         },
       :matriarch => {
-        #your key/value pairs here
+        
         },
       :hero => {
         #your key/value pairs here
